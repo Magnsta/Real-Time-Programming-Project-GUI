5512f1dbb2afd286efae3f5d360321eb3afe1754
Magnus <magnsta@stud.ntnu.no>
2020-11-09 16:35:27 +0100

    Implemented Timer to GUI

--- src/onlyboatgui/OnlyBoatGUI.java
+++ src/onlyboatgui/OnlyBoatGUI.java
@@ -2,31 +2,36 @@ package onlyboatgui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
 
 /**
  * 
  * @author stava
  */
-public class OnlyBoatGUI 
+public class OnlyBoatGUI extends javax.swing.JFrame
 {
-    
     /**
     * 
     */
-    private static void createAndSHowGUI()
-    {
+    private OnlyBoatGUI()throws IndexOutOfBoundsException
+    {     
         GUI frame = new GUI("ONLYBOAT GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         frame.setBackground(Color.yellow);
         frame.setPreferredSize(new Dimension(800,800));
         frame.pack();
         frame.setVisible(true);
-        frame.setLocationRelativeTo(null);     
-    }
-    
+        frame.setLocationRelativeTo(null);    
+   }         
+       
     /**
     * 
     * @param args 
@@ -38,7 +43,7 @@ public class OnlyBoatGUI
             @Override
             public void run() 
             {
-                createAndSHowGUI();
+               new OnlyBoatGUI();
             }
         });
     }
