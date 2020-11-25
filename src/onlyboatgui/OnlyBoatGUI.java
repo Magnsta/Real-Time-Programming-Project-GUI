a27c0ca633c1d2d7084bd44ce2ceaa64bc4109ec
Stava <magnsta@stud.ntnu.no>
2020-11-25 15:00:18 +0100

    Changed label variables to Atomic to ensure thread concurrency added missing java doc removed generic SwingWorker template

--- src/onlyboatgui/OnlyBoatGUI.java
+++ src/onlyboatgui/OnlyBoatGUI.java
@@ -2,26 +2,36 @@ package onlyboatgui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 /**
  * 
- * @author stava
+* Simple GUI for semester project in subject true-time programming
+ * Main goal of this GUI is to implement Threads and take into considertaion
+ * the concept of thread safety.
+ * This is achived through utilizing the SwingWorker class to 
+ * do the background work and updating the GUI while the GUI remains
+ * interactive for the end-user. 
+ *
+ * The visual aspect of the GUI has not been a priority
+ * @author Magnus Stava
  */
 public class OnlyBoatGUI 
 {
     
     /**
-    * 
+    * Create the GUI frame 
     */
-    private static void createAndSHowGUI()
+    private static void createAndSHowGUI() throws IOException
     {
         GUI frame = new GUI("ONLYBOAT GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.setBackground(Color.yellow);
-        frame.setPreferredSize(new Dimension(800,800));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        frame.setBackground(Color.black);
+        frame.setPreferredSize(new Dimension(1200,1000));
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);     
@@ -36,9 +46,17 @@ public class OnlyBoatGUI
         SwingUtilities.invokeLater(new Runnable() 
         {              
             @Override
+            /**
+             * Invoke run() method from Runnable interface
+             */
             public void run() 
             {
-                createAndSHowGUI();
+                try {
+                    createAndSHowGUI();
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(OnlyBoatGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
